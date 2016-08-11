7c61bcfe0e67717f87b464f58985029ef87a6581
raesylin <sylin_gibu@hotmail.com>
2016-08-11 11:59:50 -0700

    v4.01: bug fixed: fixing empty list in show page when no user input happened; instead show 10 drinks from unfiltered list

--- scripts/controllers/showCtrl.js
+++ scripts/controllers/showCtrl.js
@@ -3,6 +3,27 @@ app.controller('showCtrl', ['$scope', 'dataService', function($scope, dataServic
 	var self = this;
 
 	self.showingAll = false;
+	self.limitVal = 10; // show only 10 records if many exist
+
+	function shuffle(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		// while there remain elements to shuffle
+		while (0 !== currentIndex) {
+
+			// pick a remaining element
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// swap it with the current element
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
+
 
 	if ($scope.collection.cocktails.length === 0) {
 		dataService.getCocktails().then(function(data) {
@@ -15,6 +36,9 @@ app.controller('showCtrl', ['$scope', 'dataService', function($scope, dataServic
 	} else {
 		if ($scope.collection.sortedCocktails.length === 0) {
 			$scope.collection.sortedCocktails = $scope.collection.cocktails.slice(0);
+			// Randomize cocktail list from api or database since there is no filter 
+			shuffle($scope.collection.sortedCocktails);
+
 			self.showingAll = true;
 		}
 		console.log("Inherited cocktail list.");
